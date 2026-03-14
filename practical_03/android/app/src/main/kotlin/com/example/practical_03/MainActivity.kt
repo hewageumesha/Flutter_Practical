87faad74b16df4d60b6a9cf8631c771a120649fd
dil <n.u.m.hewage@gmail.com>
2026-03-14 12:51:50 +0530

    Flutter Project Setup

---
+++ practical_03/android/app/src/main/kotlin/com/example/practical_03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practical_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- practical_08/lib/main.dart
+++ practical_08/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practical_08/combine_animations.dart';
 import 'package:practical_08/customize_animations.dart';
-import 'package:practical_08/rotation_animation.dart';
-import 'package:practical_08/fade_animation.dart';
-import 'package:practical_08/scaler_animation.dart';
 
 void main() {
   runApp(const MainApp());
